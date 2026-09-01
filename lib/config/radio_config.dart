b77c9bbb6bde1de0cbc07e8f69d7d6f552751742
cmbenkouma-alt <cmbenkouma@gmail.com>
2026-09-01 15:48:31 +0200

    mise à jour de la configuration radio et partenaires

--- lib/config/radio_config.dart
+++ lib/config/radio_config.dart
@@ -2,16 +2,9 @@ class RadioConfig {
   static const String stationName = 'Radio Ciwara';
   static const String frequency = '105.5 FM';
   static const String city = 'Bamako';
-
-  // URL déjà présente dans le site Radio Ciwara.
-  // Elle est utilisée par le lecteur natif de l'application.
-  static const String streamUrl =
-      'https://uk5freenew.listen2myradio.com/live.mp3?typeportmount=s1_35628_stream_416941156';
-
-  // Page mobile Caster/Radio12345 utilisée sur le site.
+  static const String streamUrl = 'https://uk5freenew.listen2myradio.com/live.mp3?typeportmount=s1_35628_stream_416941156';
+  static const String websiteUrl = 'https://www.ciwara-medias.ml/';
   static const String mobilePlayerUrl = 'https://ciwarafm.radio12345.com/';
-
-  // Identifiant public du widget Caster.fm actuellement présent sur le site.
-  static const String casterPublicToken =
-      '236f6884-9ad2-465f-a29e-5f349a8dac8e';
+  static const String casterPublicToken = '236f6884-9ad2-465f-a29e-5f349a8dac8e';
+  static const String partnerUrl = 'https://hitradio.ma/';
 }
